9b9daae8d1ade709a91d4ffc6d03f14945178fc1
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-12-22 22:27:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(recipes, ingredient, quantity) {
-  var new_recipes = Object.assign({}, recipes, { ingredient: quantity });
-  //new_recipes[ingredient] = quantity;
+  var new_recipes = Object.assign({}, recipes);
+  new_recipes[ingredient] = quantity;
   return new_recipes;
 }
 
