140364e6a3ab642d8fcbbe6d9864f87c9fb634aa
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-12-22 21:38:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = {
+  onion: 3,
+};
+
+function updateObjectWithKeyAndValue(recipes, ingredient, quantity) {
+  //var new_recipes = Object.assign(recipes, { ingredient: quantity });
+  recipes[ingredient] = quantity;
+  //return new_recipes;
+  return recipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, ingredient, quantity) {
+  recipes[ingredient] = quantity;
+  return recipes;
+}
+
+function deleteFromObjectByKey(recipes, ingredient) {
+  var new_recipes = Object.assign(recipes);
+  delete new_recipes[ingredient];
+  return new_recipes;
+}
+
+function destructivelyDeleteFromObjectByKey(recipes, ingredient) {
+  delete recipes[ingredient];
+  return recipes;
+}
